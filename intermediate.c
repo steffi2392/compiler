16315e0b1c167336dc59e5501b695d056df48e7e
Stefanie L. Ostrowski <steffi@kearsarge.cs.dartmouth.edu>
2013-05-15 16:41:10 -0400

    added intermediate files

---
+++ intermediate.c
@@ -0,0 +1,36 @@
+/* intermediate.c
+ * generates intermediate code given an AST
+ */ 
+
+#import <stdio.h>
+#import "ast.h"
+#import "intermediate.h" 
+
+/* create a quad with a given opcode and return a pointer 
+   to it.  Initializes all addresses to NULL */
+quad create_quad(opcode_type opcode){
+  quad new_quad = malloc(sizeof(struct quad_struct)); 
+  new_quad->opcode = opcode; 
+  new_quad->address1 = NULL; 
+  new_quad->address2 = NULL; 
+  new_quad->address3 = NULL; 
+  new_quad->next = NULL; 
+  new_quad->prev = NULL; 
+  return new_quad; 
+}
+
+/* Create a quad_list. first is initially NULL. */ 
+quad_list create_quad_list(){
+  quad_list new_quad_list = malloc(sizeof(struct quad_list_struct)); 
+  new_quad_list->first = NULL; 
+
+  return new_quad_list; 
+}
+
+/* generates intermediate code for the node, returns a 
+   linked list of quads. */ 
+quad_list generate_intermediate(ast_node node){
+  quad_list inter_code = create_quad_list(); 
+
+  
+}
---
+++ intermediate.h
@@ -0,0 +1,39 @@
+/* intermediate.h
+ * 
+ * Defines some structs and stuff for intermediate code. 
+ * Might not end up using this, but it'll be helpful for now. 
+ * 
+ * By Steffi Ostrowski
+ */ 
+
+#ifndef INTERMEDIATE_H
+#define INTERMEDIATE_H
+
+/* opcodes for the quads */ 
+typedef enum {assn, add, sub, mult, div } opcode_type; 
+
+/* Structure for a quad. Knows its opcode and up to 3 addresses - these are strings
+   which can then be looked up in the symbol table. It also knows its next and
+   previous instructions */  
+typedef struct quad_struct *quad; 
+struct quad_struct {
+  opcode_type opcode; 
+  char * address1; 
+  char * address2; 
+  char * address3; 
+  quad next; 
+  quad prev; 
+}; 
+
+typedef struct quad_list_struct *quad_list; 
+struct quad_list {
+  quad first; 
+}
+
+/* Create a quad with a given opcode and return a pointer to that quad. */
+quad create_quad(opcode_type opcode); 
+
+/* Create a quad_list and return a pointer to it */ 
+quad_list create_quad_list(); 
+
+#endif
