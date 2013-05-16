eb4468a1d8cdf523d4bdf0aad202f41dffa94cf4
Stefanie Ostrowski <steffi.ostrowski@gmail.com>
2013-05-15 21:08:43 -0400

    updated intermediate.c

--- intermediate.c
+++ intermediate.c
@@ -6,6 +6,9 @@
 #import "ast.h"
 #import "intermediate.h" 
 
+/* Prototypes */ 
+static void generate_traverse(ast_node node, quad_list inter_code); 
+
 /* create a quad with a given opcode and return a pointer 
    to it.  Initializes all addresses to NULL */
 quad create_quad(opcode_type opcode){
@@ -32,5 +35,19 @@ quad_list create_quad_list(){
 quad_list generate_intermediate(ast_node node){
   quad_list inter_code = create_quad_list(); 
 
-  
+  generate_traverse(node, inter_code); 
+  return quad_list; 
+}
+
+/* actually does the fun stuff */ 
+static void generate_traverse(ast_node node, quad_list inter_code){
+  if (node != NULL){
+    ast_node child = node->left_child; 
+    while (node != NULL){
+      // generate code to prepare for child
+      generate_traverse(child, inter_code); 
+      child = child->right_sibling; 
+    } 
+    // generate code to implement node's action
+  }
 }
